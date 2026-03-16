ff75c0012373eb7e44ac8fd3c10f142b3ac3fe52
Popcornpopper23 <vlkandiah1s@capetigers.com>
2026-03-16 09:12:59 -0500

    Update bc_outline.js

--- js/bc_outline.js
+++ js/bc_outline.js
@@ -27,22 +27,6 @@
 
 */
 
- Function List
-   =============
-
-   makeOutline()
-      Generates the text of the table of contents
-      as a nested list
-
-   createList(source, TOCList, headings)
-      Creates an outline based on the source document,
-      list items are appended to TOCList,
-      the list items are based on the element names
-      specified in the headings array
-
-
-*/
-
 /*Generate an outline based on h1 through h6 headings in the source document*/
 
 window.addEventListener("load", makeOutline);
